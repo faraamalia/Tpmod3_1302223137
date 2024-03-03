4f254bf5e002fdebd45d23a1dc9f62b5c0b86cb8
fara <153857497+faraamalia@users.noreply.github.com>
2024-03-03 15:40:22 +0700

    First upload

---
+++ tpmod3_1302223137/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace tpmod3_1302223137
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextbox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.inputTextbox.Location = new System.Drawing.Point(123, 92);
+            this.inputTextbox.Multiline = true;
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(322, 54);
+            this.inputTextbox.TabIndex = 0;
+            this.inputTextbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(507, 100);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(115, 41);
+            this.submitButton.TabIndex = 1;
+            this.submitButton.Text = "SUBMIT";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(170, 201);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(149, 37);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "OUTPUT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.inputTextbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextbox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
